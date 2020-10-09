146af83202964cf7b3cd3b197c6c62ef11036326
Makarand03 <dahibhatemakarand@gmail.com>
2020-10-09 18:35:55 +0530

    1st code

---
+++ data types.py
@@ -0,0 +1,29 @@
+#integer
+s = 123445
+print(type(s))
+
+#float
+t= 12.145
+print(type(t))
+
+#String
+a = 'Makarand'
+print(type(a))
+
+#Boolean : Anything except 0 and Null will return a boolean value True
+b = True
+l = False
+print(type(b))
+print(type(l))
+
+#type Conversion
+# Use variable_name = data_type(value)
+c = 12.44
+d = int(10.55)
+print(type(d))
+
+#Addition of Integer and float gives float
+x = 12
+y = 13.56
+z = x+y
+print(z)
